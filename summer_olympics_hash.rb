9f20a81e079278d131cb67b61b96724406ff035d
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-16 19:08:42 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -2,42 +2,42 @@
 #year = ["1996","2000","2004","2008","2012"]
 
 # summer_olympics = {
-  :Sydney => 2000,
-  :Athens => 2004,
-  :Beijing => 2008,
-  :London => 2012,
-}
-  puts summer_olympics
+  #:Sydney => 2000,
+  #:Athens => 2004,
+#   :Beijing => 2008,
+#   :London => 2012,
+# }
+#   puts summer_olympics
   
-end
-puts create_olympics_hash
-
-def add_a_key_value_pair 
- summer_olympics = {
-  :Sydney => 2000,
-  :Athens => 2004,
-  :Beijing => 2008,
-  :London => 2012,
-}
-  summer_olympics["Atlanta"] = "1996" 
-
-  
-end
-
-# def iterate_through_hash = 
-#   place = ["Atlanta","Sydney","Athens","Beijing","London"]
-#   year = ["1996","2000","2004","2008","2012"]
-  
-#   olympics_hash = {}
-#   index = 0 
-#   place_array.each do |location|
-#     olympics_hash[location] = year_array[0]
-#     index += 19
-#   puts olympics_hash
-#   puts "The _____ summer olympics took place in _____."
-# end 
-
-# def iterate_through_keys
-# upcased_cities = [] 
- 
 # end
+# puts create_olympics_hash
+
+# def add_a_key_value_pair 
+# summer_olympics = {
+#   :Sydney => 2000,
+#   :Athens => 2004,
+#   :Beijing => 2008,
+#   :London => 2012,
+# }
+#   summer_olympics["Atlanta"] = "1996" 
+
+  
+# end
+
+def iterate_through_hash = 
+  place = ["Atlanta","Sydney","Athens","Beijing","London"]
+  year = ["1996","2000","2004","2008","2012"]
+  
+  olympics_hash = {}
+  index = 0 
+  place_array.each do |location|
+    olympics_hash[location] = year_array[0]
+    index += 19
+  puts olympics_hash
+  puts "The _____ summer olympics took place in _____."
+end 
+
+def iterate_through_keys
+upcased_cities = [] 
+ 
+end
